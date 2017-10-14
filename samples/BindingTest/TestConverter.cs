54939fb7cc73d09b259a189e12891baa6ba78a42
ImoutoChan <kirirrin@gmail.com>
2017-10-14 11:46:29 +0300

    Demo

---
+++ samples/BindingTest/TestConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Markup;
+using Avalonia.Media;
+
+namespace BindingTest
+{
+    class TestConverter : IValueConverter
+    {
+        public static TestConverter Instance { get; } = new TestConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new SolidColorBrush(Colors.AliceBlue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
